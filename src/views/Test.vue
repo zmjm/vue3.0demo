3b27bab522b2e8f4228c02069bd9c64351bcfc06
zhangzimao <zhangzimao@basestonedata.com>
2020-08-19 15:39:40 +0800

    update

---
+++ src/views/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="test">
+    <h1>test count: {{ count }}</h1>
+    <button @click="add">add</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const count = ref("'s'");
+    const add = () => {
+      count.value = count.value + "'b'";
+    };
+    return {
+      count,
+      add
+    };
+  }
+};
+</script>
